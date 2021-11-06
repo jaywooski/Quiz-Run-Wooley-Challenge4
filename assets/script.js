9ca74d44cc49a9675ff6de00ce29506c63747d2c
Woogster <john.m.wooley@gmail.com>
2021-11-06 02:57:30 -0500

    Added files and played around with styling and javascript

---
+++ assets/script.js
@@ -0,0 +1,47 @@
+// create element to represent questions
+var questionEl = document.querySelector("#question");
+
+// create element to represent answers
+var answerOptionEl = document.querySelector("#answer-opt");
+
+// sets timer to default timing
+var counter = 90;
+
+/* var questionnaire = [
+    quizObj = {
+        question: "Which number is greatest?",
+        answers: ["1","2","3"]
+    },
+    quizObj_1 = {
+        question: "Pick the lowest number",
+        answers: ["1","3","5"]
+    },
+    quizObj_2 = {
+        question: "Pick the middle number",
+        answers: ["2", "4", "6"]
+    },
+    quizObj_3 = {
+        question: "Pick the highest number",
+        answers: ["3","7", "11"]
+    },
+    quizObj_4 = {
+        question: "Pick the lowest number",
+        answers: ["1","10","100"]
+    }
+// ]; // creates array of question objects
+
+/**************************************** Functions section ****************************************************/
+var startQuiz = function() {
+
+};
+
+var askQuestion = function(questionnaire) {
+
+};
+var createQuestion = function() {
+
+}
+
+var endQuiz = function() {
+
+};
